0998de5efe1961b05fef78a11f5b0c4fe199b9cb
mv87960 <129288966+mv87960@users.noreply.github.com>
2023-06-13 14:11:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
+
+
 from flask import Flask, request, render_template
 import openai
 
 app = Flask(__name__, template_folder='templates')
+from app.utils import openai_utils
 
-openai.api_key = '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'
+
+
+openai.api_key = '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'
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/generate', methods=['GET'])
+@app.route('/generate', methods=['POST'])
 def generate_prompt():
     prompt = request.form['prompt']
     model = request.form['model']
-    
+
     if model == 'ChatGPT':
         model_name = 'gpt-3.5-turbo'
-    
+
     response = openai.Completion.create(
         engine=model_name,
         prompt=prompt,
@@ -25,10 +30,11 @@ def generate_prompt():
         stop=None,
         temperature=0.7
     )
-    
+
     answer = response.choices[0].text.strip()
-    
+
     return render_template('result.html', prompt=prompt, model=model, answer=answer)
 
 if __name__ == '__main__':
     app.run()
+
